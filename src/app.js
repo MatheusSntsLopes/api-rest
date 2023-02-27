e9419bb43900ebc84e6857c8040d625d833f0291
matheus <abbadoncruel@hotmail.com>
2023-02-27 15:45:23 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { resolve } from 'path';
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import fotoRoutes from './src/routes/fotoRoutes';
-import './src/database';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fotoRoutes from './routes/fotoRoutes';
+import './database';
 
 class App {
   constructor() {
@@ -20,7 +20,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static(resolve(__dirname, 'uploads')));
+    this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
